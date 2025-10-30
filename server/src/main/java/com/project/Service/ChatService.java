904c9565d3c857d7ef231ab691682829b9d732f8
jongsiklee01 <sononot445@gmail.com>
2025-10-30 18:04:10 +0900

    데이터 오류 찾는중...

--- server/src/main/java/com/project/Service/ChatService.java
+++ server/src/main/java/com/project/Service/ChatService.java
@@ -33,6 +33,9 @@ public class ChatService {
         // json 데이터로 답변 찾기
         DietRule matchRule = findJsonAnswer(userContext, keyword, jsonString);
 
+        system.out.println("JSON 데이터---------");
+        system.out.println(matchRule);
+
         if(matchRule != null){
             return jsonAnswer(matchRule, userContext);
         } else {
